a83a2b63640e39fe2d2c7fd827621dc3af1727c9
jh-chch <jhchoig9@gmail.com>
2024-04-08 22:44:34 +0900

    LocaleResolver CookieLocaleResolver

--- src/main/java/com/message/message/MessageController.java
+++ src/main/java/com/message/message/MessageController.java
@@ -1,18 +1,30 @@
 package com.message.message;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
 
-
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class MessageController {
 
+    @Autowired
+    LocaleResolver localeResolver;
+
     @GetMapping("/message")
-    public String getMethodName(Model model) {
+    public String getMethodName(HttpServletRequest request, HttpServletResponse response
+                        , Model model, @RequestParam String lang) {
+        localeResolver.setLocale(request, response, new Locale(lang));        
         model.addAttribute("name", "CHOI");
+
         return "message";
     }
-    
+
 }

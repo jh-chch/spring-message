0ae98b81ef2c83293f86e3cbffa118d60b93bd3e
jh-chch <jhchoig9@gmail.com>
2024-04-08 22:29:03 +0900

    타임리프에서 메시지 소스 적용

--- src/main/java/com/message/message/MessageController.java
+++ src/main/java/com/message/message/MessageController.java
@@ -1,14 +1,17 @@
 package com.message.message;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
 public class MessageController {
 
     @GetMapping("/message")
-    public String getMethodName() {
+    public String getMethodName(Model model) {
+        model.addAttribute("name", "CHOI");
         return "message";
     }
     
